9a6c5ac04293263b0f123038523c30327713a0f9
Akimenko_Ivan <412871@niuitmo.ru>
2023-12-04 21:20:01 +0300

    + HW 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src import item
+
+
+@pytest.mark.parametrize('Item, expected', [
+    (item.Item("Телевизор", 40_000, 100), 4_000_000),
+    (item.Item("Ноутбук", 60_000, 250), 15_000_000)
+])
+def test_item_calculate(Item, expected):
+    assert Item.calculate_total_price() == expected
+
